f478191550048767389e562774a9dd3192774659
Matthew Lilley <hello@matthewlilley.com>
2022-01-06 11:47:14 +0000

    feat: abstract reentrancy guard

---
+++ contracts/abstract/ReentrancyGuard.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.8.0;
+
+error Reentrancy();
+
+abstract contract ReentrancyGuard {
+    uint256 private locked = 1;
+
+    modifier nonReentrant() {
+        if (locked != 1) {
+            revert Reentrancy();
+        }
+
+        locked = 2;
+
+        _;
+
+        locked = 1;
+    }
+}
