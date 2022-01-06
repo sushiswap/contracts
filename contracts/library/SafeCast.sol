e408445fce95b00071b68754cab1f42037616d3f
Matthew Lilley <hello@matthewlilley.com>
2022-01-06 11:15:38 +0000

    feat: safecast library

---
+++ contracts/library/SafeCast.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+library SafeCast {
+    function to248(uint256 x) internal pure returns (uint248 y) {
+        require(x <= type(uint248).max);
+
+        y = uint248(x);
+    }
+
+    function to128(uint256 x) internal pure returns (uint128 y) {
+        require(x <= type(uint128).max);
+
+        y = uint128(x);
+    }
+
+    function to96(uint256 x) internal pure returns (uint96 y) {
+        require(x <= type(uint96).max);
+
+        y = uint96(x);
+    }
+
+    function to64(uint256 x) internal pure returns (uint64 y) {
+        require(x <= type(uint64).max);
+
+        y = uint64(x);
+    }
+
+    function to32(uint256 x) internal pure returns (uint32 y) {
+        require(x <= type(uint32).max);
+
+        y = uint32(x);
+    }
+}
---
+++ test/library/SafeCast.test.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+
+describe('SafeCast', function() {
+  describe('to248', function() {
+    it('Allows cast of uint256 where n is less than or equal to uint248.max', async function() {
+      //
+    })
+    it('Reverts cast of uint256 where n is more than uint248.max', async function() {
+      //
+    })
+  })
+
+  describe('to128', function() {
+    it('Allows cast of uint256 where n is less than or equal to uint128.max', async function() {
+      //
+    })
+    it('Reverts cast of uint256 where n is more than uint128.max', async function() {
+      //
+    })
+  })
+
+  describe('to96', function() {
+    it('Allows cast of uint256 where n is less than or equal to uint96.max', async function() {
+      //
+    })
+    it('Reverts cast of uint256 where n is more than uint96.max', async function() {
+      //
+    })
+  })
+
+  describe('to64', function() {
+    it('Allows cast of uint256 where n is less than or equal to uint64.max', async function() {
+      //
+    })
+    it('Reverts cast of uint256 where n is more than uint64.max', async function() {
+      //
+    })
+  })
+
+  describe('to32', function() {
+    it('Allows cast of uint256 where n is less than or equal to uint32.max', async function() {
+      //
+    })
+    it('Reverts cast of uint256 where n is more than uint32.max', async function() {
+      //
+    })
+  })
+})
